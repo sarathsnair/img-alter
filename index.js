4ff2bf18261bb999c09810b98e7b8740b8f3ddfe
Sarath S Nair <Sarath S Nair>
2016-11-26 22:11:59 +0530

    new logic for fetching all the image urls in a page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-(function () {
+(function($) {
     var ImageAlter = {
-        fetchImageAltTags: function () {
+        fetchImageAltTags: function() {
             var apikey = "e819d57ca4064af4ada5ae1cc4ff0628";
             var url = "https://api.projectoxford.ai/vision/v1.0/describe";
             /*var request = new XMLHttpRequest();
@@ -10,7 +10,7 @@
             request.send({
                 "url": "https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/42_bill_clinton.jpg"
             });
-
+            
             request.onreadystatechange = function () {
                 if (this.readyState === 4) {
                     if (this.status >= 200 && this.status < 400) {
@@ -22,27 +22,41 @@
                 }
 
             }*/
-            var obj = {
-                "url": "https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/42_bill_clinton.jpg"
-            };
-            $.ajax({
-                url: url,
-                type: "POST",
-                dataType: "json",
-                headers: {
-                    "Ocp-Apim-Subscription-Key": "e819d57ca4064af4ada5ae1cc4ff0628",
-                    "Content-Type": "application/json"
-                },
-                data: JSON.stringify(obj),
-                success: function (data) {
-                    console.log(data);
-                },
-                error: function (error) {
-                    console.log(error);
-                },
-            });
 
+            var getImageURL = function() {
+                var arr = [];
+                var imgs = document.getElementsByTagName('img');
+                for (var i = 0; i < imgs.length; i++) {
+                    arr.push(imgs[i].src);
+                }
+                return arr;
+            };
+            var images = $('img');
+            var imageurls = getImageURL();
+            for (var i = 0; i < imageurls.length; i++) {
+                var obj = {
+                    "url": imageurls[i]
+                }
+                $.ajax({
+                    url: url,
+                    type: "POST",
+                    dataType: "json",
+                    headers: {
+                        "Ocp-Apim-Subscription-Key": apikey,
+                        "Content-Type": "application/json"
+                    },
+                    data: JSON.stringify(obj),
+                    success: function(data) {
+                        console.log("URL = " + obj.url);
+                        console.log(data.description.captions[0].text);
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    },
+                });
+
+            }
         }
     }
     window.ImageAlter = ImageAlter;
-})();
+})(jQuery);
