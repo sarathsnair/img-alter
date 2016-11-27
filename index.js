9404d4ed2dff13d716011940814c9768f37da039
Sarath S Nair <Sarath S Nair>
2016-11-27 22:13:25 +0530

    Setting alt tags of image

--- index.js
+++ index.js
@@ -3,26 +3,6 @@
         fetchImageAltTags: function() {
             var apikey = "e819d57ca4064af4ada5ae1cc4ff0628";
             var url = "https://api.projectoxford.ai/vision/v1.0/describe";
-            /*var request = new XMLHttpRequest();
-            request.open('POST', url, true);
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.setRequestHeader('Access-Control-Allow-Origin','*');
-            request.send({
-                "url": "https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/42_bill_clinton.jpg"
-            });
-            
-            request.onreadystatechange = function () {
-                if (this.readyState === 4) {
-                    if (this.status >= 200 && this.status < 400) {
-                        console.log("Success");
-                        var resp = this.responseText;
-                    } else {
-                        console.log("Error");
-                    }
-                }
-
-            }*/
-
             var getImageURL = function() {
                 var arr = [];
                 var imgs = document.getElementsByTagName('img');
@@ -31,8 +11,13 @@
                 }
                 return arr;
             };
+
+            var setAltTags = function(image, altTagString) {
+                image.setAttribute('alt', altTagString);
+            };
             var images = $('img');
             var imageurls = getImageURL();
+            var arr = [];
             for (var i = 0; i < imageurls.length; i++) {
                 (function(i) {
                     var obj = {
@@ -48,8 +33,7 @@
                         },
                         data: JSON.stringify(obj),
                         success: function(data) {
-                            console.log("URL = " + obj.url);
-                            console.log(data.description.captions[0].text);
+                            setAltTags(images[i], data.description.captions[0].text);
                         },
                         error: function(error) {
                             console.log(error);
