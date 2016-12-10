2c418571141b9de5d6160674163139698ed0488e
Sarath S Nair <sarath.nair@asda.co.uk>
2016-12-10 18:04:58 +0530

    format code

--- img-alter.js
+++ img-alter.js
@@ -1,15 +1,15 @@
-(function() {
+(function () {
     "use strict";
 
     var ImageAlter = {
-        fillImageAltTags: function(obj) {
+        fillImageAltTags: function (obj) {
             if (typeof obj === null || !obj.apiKey) {
                 console.warn("img-alter needs Microsoft Vision API Key to work properly.");
                 return -1;
             }
             var apikey = obj.apiKey;
             var url = "https://api.projectoxford.ai/vision/v1.0/describe";
-            var getImageURL = function() {
+            var getImageURL = function () {
                 var arr = [];
                 var imgs = document.getElementsByTagName('img');
                 for (var i = 0; i < imgs.length; i++) {
@@ -18,14 +18,14 @@
                 return arr;
             };
 
-            var setAltTags = function(image, altTagString) {
+            var setAltTags = function (image, altTagString) {
                 image.setAttribute('alt', altTagString);
             };
             var images = document.getElementsByTagName("img");
             var imageurls = getImageURL();
             var arr = [];
             for (var i = 0; i < imageurls.length; i++) {
-                (function(i) {
+                (function (i) {
                     var obj = {
                         "url": imageurls[i]
                     }
@@ -35,7 +35,7 @@
                     xmlhttp.setRequestHeader('Access-Control-Allow-Headers', 'x-requested-with');
                     xmlhttp.setRequestHeader("Ocp-Apim-Subscription-Key", apikey);
                     xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-                    xmlhttp.onreadystatechange = function() {
+                    xmlhttp.onreadystatechange = function () {
                         if (xmlhttp.readyState == XMLHttpRequest.DONE && xmlhttp.status == 200) {
                             //console.log(xmlhttp.responseText);
                             setAltTags(images[i], JSON.parse(xmlhttp.responseText).description.captions[0].text);
