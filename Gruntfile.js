72de4e6fee8a156c45c7b4918111ac9954060edc
Sarath S Nair <sarathsnairxyz@gmail.com>
2016-12-10 18:28:55 +0530

    Added travis CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,5 +13,5 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('build', ['uglify']);
 };
